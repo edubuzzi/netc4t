c3999ea2ed29ad6d3bbdcc4385824743ab8b150f
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-29 20:32:53 -0300

    Update sender-chat.sh

--- sender-chat.sh
+++ sender-chat.sh
@@ -3,7 +3,7 @@
 echo
 read -p "Informe o IP que está escutando: " IPlistening
 read -p "Informe a porta para escutar (entre 1-65535): " PORTA
-echo "O NetCAT vai tentar se comunicar com a porta $PORTA do $IPlistening"
+echo "Tentando se comunicar com a porta $PORTA do $IPlistening"
 echo
 nc $IPlistening $PORTA
 ./execute.sh
