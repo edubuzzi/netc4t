2ecfd70a2b4f1a5b71e98dab8ebea7895a459d6e
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-29 19:38:30 -0300

    Update listener-chat-interno-tcp.sh

--- listener-chat-interno-tcp.sh
+++ listener-chat-interno-tcp.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+IPinterno=$(hostname -I | cut -d " " -f1)
+
+echo
+echo "Seu IP Interno é: $IPinterno"
+echo
+read -p "Informe a porta para escutar (entre 1-65535): " PORTA
+nc -lvp $PORTA
+exit
