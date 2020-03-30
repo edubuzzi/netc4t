7d8ea228264c4128d9a311ecf80ecd7d8acf6bcd
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-29 23:56:28 -0300

    Update portscan-dominio.sh

--- portscan-dominio.sh
+++ portscan-dominio.sh
@@ -2,17 +2,22 @@
 echo
 read -p "Informe o dominio: " DOMINIO
 IP=$(host $DOMINIO | cut -d ' ' -f4 | head -n1)
-if [ -z $IP ]
+if [ $IP = 'found:' ]
 then
-echo "O domínio $DOMINIO aparentemente está fora do ar!"
-echo "Tente novamente!!"
+echo "O dominio $DOMINIO aparentemente está fora do ar!"
+echo "Verifique o dominio novamente ou coloque um válido!!"
 ./portscan-dominio.sh
 exit
 fi
-echo "Informe o delimitador de portas quer fazer o scan (ex: 21-443)"
+echo "Informe o delimitador de portas quer fazer o scan (ex: 21 443)"
 echo "Se quiser fazer scan de apenas uma porta só coloca ela sozinha (ex: 80)"
-read -p "Informe a(s) porta(s) para fazer scan: " PORTA
-if [ -z $PORTA ]
+read -p "Informe a(s) porta(s) para fazer scan: " PORTAinicial PORTAfinal
+if [ -z $PORTAinicial ] && [ -z $PORTAfinal ]
 then
-$PORTA="1-1000"
+echo "Por que você não informou nenhuma porta?"
+echo "Tudo bem, vou fingir que informou a porta 80 ;)"
+$PORTAinicial="80"
 fi
+nc -vzn -w1 $IP $PORTAinicial-$PORTAfinal
+./execute.sh
+exit
