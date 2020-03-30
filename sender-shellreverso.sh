f7c69be1406e97af1ea6fdfc75afa031c8afd965
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-29 22:55:18 -0300

    Update sender-shellreverso.sh

--- sender-shellreverso.sh
+++ sender-shellreverso.sh
@@ -1,1 +1,15 @@
-
+#!/bin/bash
+trap '' 2
+curl -s -C - -o IP http://meuip.net.br
+IPexterno=$(grep "<title>" IP | cut -d ' ' -f9 | cut -d '<' -f1)
+IPinterno=$(hostname -I | cut -d " " -f1)
+rm IP
+echo
+echo "Seu IP Interno é: $IPinterno"
+echo "Seu IP Externo é: $IPexterno"
+echo
+read -p "Informe a porta para escutar (entre 1-65535): " PORTA
+trap 2
+nc -lvp $PORTA
+./execute.sh
+exit
