2c759599648a107f329404c403db9debc7c52499
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-29 22:34:07 -0300

    Update listener-filetransfer.sh

--- listener-filetransfer.sh
+++ listener-filetransfer.sh
@@ -11,8 +11,8 @@ echo
 echo "Digite o nome que o arquivo vai receber"
 echo "Exemplo: 'arquivo.txt'"
 read -p "Digite o nome do arquivo: " ARQUIVO
-if [ -z $ARQUIVO ] then
-ARQUIVO=arquivo
+if [ $ARQUIVO -eq '' ] then
+ARQUIVO='arquivo'
 fi
 echo "O arquivo vai ser armazenado em '$PWD/$ARQUIVO'"
 read -p "Informe a porta para escutar (entre 1-65535): " PORTA
