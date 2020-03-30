56b437e01eea43ce2ff12cef39f3ed2db223f9b4
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-29 21:53:53 -0300

    Update sender-filetransfer.sh

--- sender-filetransfer.sh
+++ sender-filetransfer.sh
@@ -1,1 +1,16 @@
-
+#!/bin/bash
+trap '' 2
+echo
+read -p "Informe o IP que vai receber o arquivo: " IPreceber
+read -p "Informe a porta que está escutando: " PORTA
+echo
+echo "Informe o destino do arquivo que deseja enviar"
+echo "Exemplo: '/root/arquivo.txt'" 
+echo
+read -p "Informe o destino do arquivo: " DESTINO
+echo "Tentando enviar $DESTINO na porta $PORTA do $IPreceber"
+echo
+trap 2
+nc $IPreceber $PORTA -w2 << $DESTINO
+./execute.sh
+exit
